c67e2f9b79ca13b1c948b4d595d320280c914b42
Anand R <anandr72@gmail.com>
2018-08-27 09:39:51 +0530

    Changes in Verified

--- src/main/java/com/training/example/JacocoExample/Palindrome.java
+++ src/main/java/com/training/example/JacocoExample/Palindrome.java
@@ -6,10 +6,16 @@ public class Palindrome {
 	    if (inputString.length() == 0) {
 	        return true;
 	    } else {
-	        char firstChar = inputString.charAt(0);
-	        char lastChar = inputString.charAt(inputString.length() - 1);
-	        String mid = inputString.substring(1, inputString.length() - 1);
-	        return (firstChar == lastChar) && isPalindrome(mid);
+	    	String rev = "";
+	    	int length = inputString.length();
+	    	 
+	        for ( int i = length - 1; i >= 0; i-- )
+	           rev = rev + inputString.charAt(i);
+	   
+	        if (inputString.equals(rev))
+	           return true;
+	        else
+	           return false;
 	    }
 	}
 
--- src/test/java/com/training/example/JacocoExample/PalindromeTest.java
+++ src/test/java/com/training/example/JacocoExample/PalindromeTest.java
@@ -11,4 +11,16 @@ public class PalindromeTest {
 	    Palindrome palindromeTester = new Palindrome();
 	    assertTrue(palindromeTester.isPalindrome(""));
 	}
+
+	@Test
+	public void check_palindrome_true() {
+	    Palindrome palindromeTester = new Palindrome();
+	    assertTrue(palindromeTester.isPalindrome("abcba"));
+	}
+	
+	@Test
+	public void check_palindrome_false() {
+	    Palindrome palindromeTester = new Palindrome();
+	    assertFalse(palindromeTester.isPalindrome("Anand"));
+	}
 }
